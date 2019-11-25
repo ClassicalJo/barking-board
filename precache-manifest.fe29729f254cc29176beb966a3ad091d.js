31ef989d9d9b4807e746e4d1e9aade04cab6f476
ClassicalJo <jb.barrientos@yahoo.com>
2019-11-26 00:47:00 +1100

    Updates

---
+++ precache-manifest.fe29729f254cc29176beb966a3ad091d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4877133156d120e406d7211f451a0765",
+    "url": "/barking-board/index.html"
+  },
+  {
+    "revision": "c0e338371ab3df027d9e",
+    "url": "/barking-board/static/css/main.2f177412.chunk.css"
+  },
+  {
+    "revision": "78bedfed5f9f55acf97a",
+    "url": "/barking-board/static/js/2.fd111386.chunk.js"
+  },
+  {
+    "revision": "c0e338371ab3df027d9e",
+    "url": "/barking-board/static/js/main.9e58858a.chunk.js"
+  },
+  {
+    "revision": "07a9296d3efd1ecdad0a",
+    "url": "/barking-board/static/js/runtime-main.d541fa05.js"
+  }
+]);
