4cefe44280c7b44c98f5c75a1d5b6ec5d61b716c
ClassicalJo <jb.barrientos@yahoo.com>
2019-11-25 13:05:45 +1100

    additional canvas
    added coins for testing.
    canvas size now a state
    styles now part of component didmount
    coins cannot go negative

--- src/BuyStyle.js
+++ src/BuyStyle.js
@@ -3,21 +3,22 @@ import React from 'react'
 class BuyStyle extends React.Component {
     render() {
         let stylesKeys = Object.keys(this.props.styles)
-        let mappedStylesKeys = stylesKeys.map((key) => { return [key, this.props.styles[key]] })
+        let mappedStylesKeys = stylesKeys.map((key) => { return [key, this.props.styles[key].bought] })
+        console.log (mappedStylesKeys)
         stylesKeys.shift()
 
         return (
             <div>
-                <div>{stylesKeys.map((x) => <div
+                <div>{stylesKeys.map((x) => <div><input
                     onClick={this.props.onClick}
-                    buystyle={x}>
-                    Buy {x} for 10 borkcoins
-                </div>)}
-                    <div>
-                        {mappedStylesKeys.map((x) => x[1]===true && <div>and buy filled {x[0]} for moar money!</div>)}
-                    </div>
+                    buystyle={x}
+                    value={`Buy ${x} for 10 borkcoins`}
+                    disabled={this.props.styles[x].bought}
+                    /></div>)}
                 </div>
-
+                <div>
+                    {mappedStylesKeys.map((x) => x[1] === true && <div>and buy filled {x[0]} for moar money!</div>)}
+                </div>
             </div>
         )
     }
